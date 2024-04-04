30657b9bb64b589ab5f6ea469105691198e9501f
RafaTPz <camarodorato@gmail.com>
2024-04-04 17:47:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import {BrowserRouter} from 'react-router-dom'
+import Rotas from './Rotas.jsx'
+
+function App() {
+  return (
+kj
+  );
+}
+
+export default App;
---
+++ src/Rotas.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+export default function Rotas(){
+    <Routes>
+        <Route 
+    </Routes>
+}
