adfff41e37b93d993f82da29694face69e6e3fe1
RafaTPz <camarodorato@gmail.com>
2024-04-05 17:34:45 -0300

    dar uma fuçada dps

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,49 @@
-import React from "react";
-import {AreaHome, Container} from './styled'
+import React, { useState, useEffect } from "react";
+import {AreaHome,AreaVoz,Container,Button} from './styled'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 const Home = () => {
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+
+    const [counter, setCounter] = useState(0);
+    const [timer, setTimer] = useState(null);
+
+    useEffect(() => {
+        if (listening) {
+            setTimer(setInterval(() => setCounter(prevCounter => prevCounter + 1), 1000));
+        } else {
+            clearInterval(timer);
+        }
+
+        return () => clearInterval(timer);
+    }, [listening]);
+
+    const resetTranscriptHandler = () => {
+        resetTranscript();
+        setCounter(0);
+    };
+
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
+
     return(
         <AreaHome>
-         <Container>
-            <h1>oiiiie</h1>
-         </Container>
+            <Container>
+                <AreaVoz>
+                    <br /><p>Microfone: {listening ? 'on' : 'off'}</p>
+                    {listening && <p>Tempo corrido: {counter} segundos</p>}
+                    <Button onClick={SpeechRecognition.startListening}>Start</Button>
+                    <Button onClick={SpeechRecognition.stopListening}>Pause</Button>
+                    <Button onClick={resetTranscriptHandler}>Reset</Button>
+                    <p>{transcript}</p>
+                </AreaVoz>
+            </Container>
         </AreaHome>
     );
 }
--- src/pages/Home/styled.jsx
+++ src/pages/Home/styled.jsx
@@ -1,14 +1,39 @@
 import styled from 'styled-components'
 
 export const AreaHome = styled.div`
- *{
-  align-items: center;
-  justify-content: center;
-}
+  font-family: Arial, sans-serif;
 `;
+
+export const AreaVoz = styled.div`
+  background-color: #f0f0f0;
+  padding: 20px;
+  border-radius: 5px;
+  margin-top: 20px;
+`;
+
 export const Container = styled.div`
-background-color: whitesmoke;
-text-align: center;
-margin-left: 10%;
-margin-right: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Button = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin-right: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
 `;
